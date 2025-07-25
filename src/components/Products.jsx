0b50b206f472e51bc26ef922b71c1e71c45ac57e
Harsh <126139439+palhrsh09@users.noreply.github.com>
2025-07-25 11:09:19 +0530

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,183 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addCart } from "../redux/action";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import toast from "react-hot-toast";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  const componentMounted = useRef(true);
+
+  const dispatch = useDispatch();
+
+  const addProduct = (product) => {
+    dispatch(addCart(product));
+  };
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products/");
+      if (componentMounted.current) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+      }
+      return () => {
+        componentMounted.current = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-12 py-5 text-center">
+          <Skeleton height={40} width={560} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+          <Skeleton height={592} />
+        </div>
+      </>
+    );
+  };
+
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((item) => item.category === cat);
+    setFilter(updatedList);
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons text-center py-5">
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewelery
+          </button>
+          <button
+            className="btn btn-outline-dark btn-sm m-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronics
+          </button>
+        </div>
+
+        {filter.map((product) => {
+          const isOutOfStock = Math.random() > 0.5; // Simulating stock status
+
+          return (
+            <div
+              id={product.id}
+              key={product.id}
+              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+            >
+              <div className="card text-center h-100" key={product.id}>
+                <div style={{ height: 300, overflow: "hidden" }}>
+                  <img
+                    className="card-img-top p-3"
+                    src={product.image}
+                    alt={product.title}
+                    style={{ objectFit: "cover", width: "100%", height: "100%" }}
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {product.title.substring(0, 12)}...
+                  </h5>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item lead">$ {product.price}</li>
+                </ul>
+                <div className="card-body">
+                  <select
+                    className="form-select mb-2"
+                    defaultValue="Size: M"
+                  >
+                    <option value="Size: S">Size: S</option>
+                    <option value="Size: M">Size: M</option>
+                    <option value="Size: L">Size: L</option>
+                    <option value="Color: Black">Color: Black</option>
+                    <option value="Color: Blue">Color: Blue</option>
+                  </select>
+                  {isOutOfStock ? (
+                    <button className="btn btn-dark m-1" disabled>
+                      Out of Stock
+                    </button>
+                  ) : (
+                    <button
+                      className="btn btn-dark m-1"
+                      onClick={() => {
+                        toast.success("Added to cart");
+                        addProduct(product);
+                      }}
+                    >
+                      Add to Cart
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+  return (
+    <>
+      <div className="container my-3 py-3">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="display-5 text-center">Latest Products</h2>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Products;
